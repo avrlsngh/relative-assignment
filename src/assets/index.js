146d9f19954d8971ee1950c21ae264801283b886
Aviral Singh <aviralsingh@Avirals-MacBook-Pro.local>
2023-03-21 23:55:33 +0530

    added icons

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import BitcoinIcon from './icons/bitcoinIcon.png'
+import SolanaIcon from './icons/solanaIcon.png'
+import EthereumIcon from './icons/ethereumIcon.png'
+import BinanceIcon from './icons/binanceIcon.png'
+import ShibaInuIcon from './icons/shibaInuIcon.png'
+import TrendingIcon from './icons/trendingIcon.png'
+
+export {
+    BitcoinIcon,
+    SolanaIcon,
+    EthereumIcon,
+    BinanceIcon,
+    ShibaInuIcon,
+    TrendingIcon
+}
